fc9236ef470fecd420d9b3224c10d90149d7b24c
Aria Taylor <ari@aricodes.net>
2024-03-21 11:43:09 -0700

    Add stats endpoint for leaderboads

--- web/controllers/vore.go
+++ web/controllers/vore.go
@@ -15,6 +15,7 @@ func (v *VoreController) Mount(baseGroup gin.IRouter) {
 	us := baseGroup.Group("vore")
 	us.GET("/", v.All)
 	us.GET("/:id", v.GetOne)
+	us.GET("/stats", v.Stats)
 }
 
 func (v *VoreController) All(c ctx) {
@@ -37,4 +38,23 @@ func (v *VoreController) GetOne(c ctx) {
 	}
 
 	c.JSON(http.StatusOK, entry)
+}
+
+func (v *VoreController) Stats(c ctx) {
+	var results []struct {
+		UserID string
+		Total  int
+	}
+
+	Vore := query.Vore
+	err := Vore.
+		Select(Vore.UserID, Vore.UserID.Count().As("total")).
+		Group(Vore.UserID).
+		Order(Vore.UserID.Count().Desc()).
+		Scan(&results)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+	}
+
+	c.JSON(http.StatusOK, results)
 }
