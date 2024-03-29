76bb76de89521e4a93df051e043086705249b617
Aria Taylor <ari@aricodes.net>
2024-03-29 15:06:46 -0700

    Delete old frontend

--- frontend.bak/src/api/discord.js
+++
@@ -1,5 +0,0 @@
-import ky from 'ky-universal';
-
-const base = '/api/discord';
-
-export const userInfo = async id => await ky.get(`${base}/user/${id}`).json();
--- frontend.bak/src/hooks/query-hooks/vore/useStats.js
+++
@@ -1,6 +0,0 @@
-import { stats } from '@/api/vore';
-import { useQuery } from '@tanstack/react-query';
-
-const useStatsQuery = () => useQuery({ queryKey: ['vore', 'stats'], queryFn: stats });
-
-export default useStatsQuery;
