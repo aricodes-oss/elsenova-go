e58b5b014917ad0882fae8895099405f7e214339
Aria Taylor <ari@aricodes.net>
2024-04-01 11:32:21 -0700

    Better timeout logging

--- cmd/serve.go
+++ cmd/serve.go
@@ -61,14 +61,15 @@ var serveCmd = &cobra.Command{
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		if err := srv.Shutdown(ctx); err != nil {
-			log.Fatal().Err(err).Msg("Server shutdown")
+			log.Fatal().Err(err).Msg("Server shutdown failure")
 		}
 
 		select {
 		case <-ctx.Done():
-			log.Warn().Msg("Sever shutdown timeout hit")
+			log.Info().Msg("Server exiting!")
+		case <-time.After(5 * time.Second):
+			log.Warn().Msg("Server shutdown timeout hit, force-killing")
 		}
-		log.Info().Msg("Server exiting!")
 	},
 }
 
